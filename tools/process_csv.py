5341a062fec7764d0601f382307cf3b1beac8550
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-05-24 21:04:53 +0100

    Update processing script

--- tools/process_csv.py
+++ tools/process_csv.py
@@ -3,14 +3,27 @@
 import argparse
 import pandas as pd
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Process CSVs containing microbenchmark performance data")
+    parser = argparse.ArgumentParser(
+        description="Process CSVs containing microbenchmark performance data")
     parser.add_argument('files', nargs='*')
+    parser.add_argument('--sort', default=True, action='store_true')
     args = parser.parse_args()
     df = pd.DataFrame()
     for f in args.files:
         df = pd.concat([df, pd.read_csv(f, sep=',\s+', engine='python')])
 
-    print(df.groupby('exe_name')['runtime'].mean())
+    column = df.groupby('exe_name')['runtime']
+    series = [column.min().rename("min"),
+              column.mean().rename("mean"),
+              column.std().rename("std")]
+    df = pd.concat(series, axis=1)
+
+    if args.sort:
+        print(df.sort_values(by='mean'))
+    else:
+        print(df)
+
 
 main()
